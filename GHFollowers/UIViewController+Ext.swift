68154294e535475afe1cc9aa31f632836bb3e560
Rodrigo Lemos <rodrigodev7@gmail.com>
2020-01-13 18:13:52 -0400

    Created Reusable Custom Alert

---
+++ GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Lemos on 08/01/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+}
