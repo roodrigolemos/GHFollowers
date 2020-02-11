47324a9805dea43cbfb85bac96d16f0dff319741
Rodrigo Lemos <rodrigodev7@gmail.com>
2020-02-10 21:42:30 -0400

    Modal Presentation & passing data

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Lemos on 10/02/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+    
+
+}
