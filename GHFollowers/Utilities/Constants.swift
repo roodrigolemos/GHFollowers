6482c52d3c2c9dfe3ed2b1488662f34a8c945f2a
Rodrigo Lemos <rodrigodev7@gmail.com>
2020-02-13 09:45:36 -0400

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Lemos on 13/02/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mapping.and.ellipse"
+}
