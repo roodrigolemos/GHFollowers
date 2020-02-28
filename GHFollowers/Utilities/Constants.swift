5ea73060a4047528f22e11f9ecfff0487cbe4578
RodrigoLemos <rodlemoss@gmail.com>
2020-02-28 16:52:49 -0400

    Search, pagination, empty state, constante and more 1 part

--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -10,15 +10,17 @@ import Foundation
 import UIKit
 
 enum SFSymbols {
-    static let location = "mapping.and.ellipse"
-    static let repos = "folder"
-    static let gists = "text.alignleft"
-    static let followers = "heart"
-    static let following = "person.2"
+    static let location = UIImage(named: "mapping.and.ellipse")
+    static let repos = UIImage(named: "folder")
+    static let gists = UIImage(named: "text.alignleft")
+    static let followers = UIImage(named: "heart")
+    static let following = UIImage(named: "person.2")
 }
 
 enum Images {
+    static let placeholder = UIImage(named: "avatar-placeholder")
     static let ghLogo = UIImage(named: "gh-logo")
+    static let emptyStateLogo = UIImage(named: "empty-state-logo")
 }
 
 enum ScreenSize {
